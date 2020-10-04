c61d7815a0d9ef03f17e057b20eca2cfbd7fe3eb
Harsh Chandravanshi <harsh0111chandravanshi@gmail.com>
2020-10-04 14:51:46 +0530

    Updates

---
+++ precache-manifest.ad9dad90f2f6baa9e19f2baa748377c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80dce9739000247be2159e394ff525a9",
+    "url": "/Reactalculator/index.html"
+  },
+  {
+    "revision": "982c140a2be4b85a15e0",
+    "url": "/Reactalculator/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "62277327ebb1dd04952b",
+    "url": "/Reactalculator/static/css/main.02251127.chunk.css"
+  },
+  {
+    "revision": "982c140a2be4b85a15e0",
+    "url": "/Reactalculator/static/js/2.18b84c87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Reactalculator/static/js/2.18b84c87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62277327ebb1dd04952b",
+    "url": "/Reactalculator/static/js/main.c2f21028.chunk.js"
+  },
+  {
+    "revision": "5438f836b97fbbafac85",
+    "url": "/Reactalculator/static/js/runtime-main.300a5344.js"
+  }
+]);
